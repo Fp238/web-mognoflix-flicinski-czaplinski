6a9b0271c5b6bb8d8548c7b5fdde650f840f989f
Filip <filip.flicinski@cyfroweszkoly.pl>
2026-02-25 19:23:10 +0100

    write loading data script in seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,1 +1,18 @@
+const fs = require("fs");
 
+
+db.filmy.deleteMany({});
+db.uzytkownicy.deleteMany({});
+db.recenzje.deleteMany({});
+
+const movies = JSON.parse(fs.readFileSync("data/movies.json", "utf-8"));
+db.filmy.insertMany(movies);
+
+const users = JSON.parse(fs.readFileSync("data/users.json", "utf-8"));
+db.uzytkownicy.insertMany(users);
+
+const ratings = JSON.parse(fs.readFileSync("data/ratings.json", "utf-8"));
+db.recenzje.insertMany(ratings);
+
+
+print("Data loaded");
