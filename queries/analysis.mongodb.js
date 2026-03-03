fb47ba30767a0c0fb94e88aecbf2a8550bb60a16
Turas <182771626+Turas39@users.noreply.github.com>
2026-03-03 21:33:56 +0100

    dodanie agregacji

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -4,3 +4,18 @@ db = db.getSiblingDB('filmydb');
 print("\n1. Filmy z danego gatunku:");
 const moviesGenres = db.filmy.find( { gatunki: { $in: ["Dramat","Akcja"] } }, { tytul: 1, data_premiery: 1, gatunki: 1, _id: 0 } );
 printjson(moviesGenres);
+
+//Agregacja: Średnia ocena dla każdego gatunku
+print("\n2. Średnia ocena dla każdego gatunku:");
+const genresAvg = db.filmy.aggregate([
+    { $unwind: "$gatunki" },
+    {
+        $group: {
+            _id: "$gatunki",
+            srednia_ocena: { $avg: "$srednia_ocen" },
+            liczba_filmow: { $sum: 1 }
+        }
+    },
+    { $sort: { srednia_ocena: -1 } }
+])
+printjson(genresAvg);
